460ba3e1fbc59143dede8d47fe25644620c04260
Matheus <matheusnizoli@gmail.com>
2025-01-22 10:09:13 -0300

    fix create table for mysql < 8.0.12

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ type server struct {
 
 var (
 	address     = flag.String("address", "0.0.0.0", "Bind IP Address")
-	port        = flag.String("port", "8080", "Listen Port")
+	port        = flag.String("port", "9000", "Listen Port")
 	waDebug     = flag.String("wadebug", "", "Enable whatsmeow debug (INFO or DEBUG)")
 	logType     = flag.String("logtype", "console", "Type of log output (console or json)")
 	colorOutput = flag.Bool("color", false, "Enable colored output for console logs")
@@ -108,7 +108,7 @@ func main() {
 			qrcode VARCHAR(255) NOT NULL DEFAULT '',
 			connected TINYINT DEFAULT 0,
 			expiration BIGINT,
-			events TEXT NOT NULL DEFAULT 'All'
+			events TEXT NOT NULL
 		);`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
